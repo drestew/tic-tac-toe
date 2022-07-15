43e345fe46104f2593b9de2d7daca6d171d9eea0
drestew <drestew48@gmail.com>
2022-07-14 20:39:35 -0400

    Add gameboard UI

---
+++ index.js
@@ -0,0 +1,9 @@
+const gameBoard = (() => {
+    const board = ['topL, topC, topR, midL, midC, midR, botL, botC, botR']
+    return { board }
+})
+
+const displayController = (() => {
+    const user = "x"
+    const computer = "o"
+})
