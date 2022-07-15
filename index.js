ddf57d336a11286f6d61b7f4b74252542b9fa1b7
drestew <drestew48@gmail.com>
2022-07-14 20:42:59 -0400

    Add gameboard module

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const gameBoard = (() => {
     const board = ['topL, topC, topR, midL, midC, midR, botL, botC, botR']
     return { board }
-})
+})()
 
 const displayController = (() => {
     const user = "x"
