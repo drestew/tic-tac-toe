2d89b7bea03e9df09c20f67f5a8057ab65d3196a
drestew <drestew48@gmail.com>
2022-07-16 22:32:46 -0400

    Add X when square is clicked

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const gameBoard = (() => {
-    const board = ['topL, topC, topR, midL, midC, midR, botL, botC, botR']
-    return { board }
+    const board = ['topL', 'topC', 'topR', 'midL', 'midC', 'midR', 'botL', 'botC', 'botR']
+
+    const topL = document.querySelector('.board__topL')
+    const topC = document.querySelector('.board__topC')
+    const topR = document.querySelector('.board__topR')
+    const midL = document.querySelector('.board__midL')
+    const midC = document.querySelector('.board__midC')
+    const midR = document.querySelector('.board__midR')
+    const botL = document.querySelector('.board__botL')
+    const botC = document.querySelector('.board__botC')
+    const botR = document.querySelector('.board__botR')
+
+    return {
+        board,
+        topL,
+        topC,
+        topR,
+        midL,
+        midC,
+        midR,
+        botL,
+        botC,
+        botR
+    }
 })()
 
-const displayController = (() => {
-    const user = "x"
-    const computer = "o"
-})
+const selectSquare = function () {
+    gameBoard.topL.textContent = "X"
+}
+
+const playGame = () => {
+    gameBoard.topL.addEventListener('click', selectSquare)
+}
+
+playGame()
